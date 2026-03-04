f20e411b5d6b88baaec7ca528c85486294753b28
FranciszekJanicki <franciszek.janicki1@gmail.com>
2026-03-04 13:07:17 +0100

    changes

--- wzum/lab1/zad8.py
+++ wzum/lab1/zad8.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import sklearn
 from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("training_data.txt", header=None, names=["charged_time", "battery_lasted"])
@@ -8,9 +9,7 @@ print(df.head())
 data = df[["charged_time"]]
 target = df["battery_lasted"]
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
+data_train, data_test, target_train, target_test = sklearn.model_selection.train_test_split(data, target, test_size=0.2)
 
-print("Train set size:", X_train.shape)
-print("Test set size:", X_test.shape)
+print(data_train)
+print(data_test)
